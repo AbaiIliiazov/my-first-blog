9d5899700ad722f2512eb7ceeae5bae28ccfe6f9
AbaiIliiazov <kivamolbulak@gmail.com>
2021-02-14 11:41:46 +0600

    My Django Girls App,first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ul$zp8m4a@_&fy)f1itduir3x+%u3@_f!-^h6-qk0xyzpxt)3k'
+SECRET_KEY = os.environ['ul$zp8m4a@_&fy)f1itduir3x+%u3@_f!-^h6-qk0xyzpxt)3k']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
