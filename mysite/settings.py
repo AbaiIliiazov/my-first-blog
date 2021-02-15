2de2e26875f5c165900ef35f4f10ef4804dc0c61
AbaiIliiazov <kivamolbulak@gmail.com>
2021-02-15 05:35:53 +0000

    overwrite

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ul$zp8m4a@_&fy)f1itduir3x+%u3@_f!-^h6-qk0xyzpxt)3k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
